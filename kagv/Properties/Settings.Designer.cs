e58338a826d8a76cb5dea07936748bd4887d9894
Dimitris Katikaridis <dkatikaridis@gmail.com>
2017-02-28 05:13:36 -0700

    project:Fix broken namespaces
    creating a Form class it was getting a wrong namespace and we had to
    change it manually

--- kagv/Properties/Settings.Designer.cs
+++ kagv/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace kaGv2.Properties {
+namespace kagv.cs.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
