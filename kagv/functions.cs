fa779438702535d44a8be1ebb3fe00a712820504
Dimitris Katikaridis <dkatikaridis@gmail.com>
2017-05-28 18:21:04 +0300

    functions:remove unused <T>

--- kagv/functions.cs
+++ kagv/functions.cs
@@ -513,17 +513,6 @@ namespace kagv {
 
         }
 
-        T[,] ResizeArray<T>(T[,] original, int rows, int cols) {
-            var newArray = new T[rows, cols];
-            int minRows = Math.Min(rows, original.GetLength(0));
-            int minCols = Math.Min(cols, original.GetLength(1));
-            for (int i = 0; i < minRows; i++)
-                for (int j = 0; j < minCols; j++)
-                    newArray[i, j] = original[i, j];
-
-            return newArray;
-        }
-
         private void updateBorderVisibility(bool hide) {
             if (hide) {
                 for (int i = 0; i < Constants.__WidthBlocks; i++)
