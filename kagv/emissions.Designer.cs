19adaf5d32d443709de75e8949d6a5e9b763efc0
Dimitris Katikaridis <dkatikaridis@gmail.com>
2017-06-29 13:01:57 +0300

    emissions:Do not allow to close/minimize/maximize

--- kagv/emissions.Designer.cs
+++ kagv/emissions.Designer.cs
@@ -89,6 +89,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "emissions";
             this.Text = "emissions";
+            this.Load += new System.EventHandler(this.emissions_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- kagv/emissions.cs
+++ kagv/emissions.cs
@@ -13,5 +13,21 @@ namespace kagv {
         public emissions() {
             InitializeComponent();
         }
+        
+        protected override CreateParams CreateParams {
+            get {
+                CreateParams param = base.CreateParams;
+                param.ClassStyle = param.ClassStyle | 0x200;
+                return param;
+            }
+        }
+
+        private void emissions_Load(object sender, EventArgs e) {
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+        }
+
+      
+        
     }
 }
