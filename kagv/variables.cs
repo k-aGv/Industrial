aa3150083f9fc44ee48b364dd59fd7d0c420caa0
Yannis <johnmenex@gmail.com>
2017-09-26 17:00:50 +0300

    More cleaning up

--- kagv/variables.cs
+++ kagv/variables.cs
@@ -21,18 +21,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
-using System.Diagnostics;
-using System.IO;
 
 namespace kagv
 {
